b5dc8adf1775861c9842fe0d2f9e683aaf94ec1a
Kasper Blom <kasper.blom@eyevinn.se>
2025-04-22 13:21:58 +0200

    Added a Swift demo app. Just load it in Xcode. Alter the URL to the stream to play and the URL to the event sink.

---
+++ Sample/SEPA_Demo/SEPA_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEPA_DemoApp.swift
+//  SEPA_Demo
+//
+//  Created by Kasper Blom on 2025-04-22.
+//
+
+import SwiftUI
+
+@main
+struct SEPA_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
